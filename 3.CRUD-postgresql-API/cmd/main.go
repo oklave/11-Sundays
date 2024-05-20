a333e316607fb33db41740814425fc546985b5de
oklave <vvalko2k20@gmail.com>
2024-05-20 22:14:54 +0400

    add 3s sundays

---
+++ 3.CRUD-postgresql-API/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
